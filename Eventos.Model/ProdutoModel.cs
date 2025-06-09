4558e71fb6c465feadb13ba1638c65352edc24e9
ALUNO SESI SENAI <ALUNO SESI SENAI@SSIRT01LABF19>
2025-06-09 11:39:26 -0300

    teste

---
+++ Eventos.Model/ProdutoModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Eventos.Model
+{
+    public class Produto
+    {
+        public int IdCliente { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+        public string Cep { get; set; }
+        public int NumResidencia { get; set; }
+        public int IdRua { get; set; }
+        public string RuaNome { get; set; }
+        public int IdBairro { get; set; }
+        public string BairroNome { get; set; }
+        public int IdCidade { get; set; }
+        public string CidadeNome { get; set; }
+    }
+}
